e53caa0e02f83c0914d6f2be33e152b2c29b5c4f
Andrii Tomashevskyi <tomashevskiy.a@gmail.com>
2018-09-02 15:00:44 +0300

    Removing Other View Engines

--- Views/Startup.cs
+++ Views/Startup.cs
@@ -14,6 +14,7 @@ namespace Views
         {
             services.AddMvc();
             services.Configure<MvcViewOptions>(options => {
+                options.ViewEngines.Clear();
                 options.ViewEngines.Insert(0, new DebugDataViewEngine());
             });
         }
