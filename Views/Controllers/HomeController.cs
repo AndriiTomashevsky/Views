60ec7eea69fd6010e331f1cab10445785ec6641e
Andrii Tomashevskyi <tomashevskiy.a@gmail.com>
2018-09-02 14:59:41 +0300

    Creating a Custom View Engine

---
+++ Views/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Views.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            ViewBag.Message = "Hello, World";
+            ViewBag.Time = DateTime.Now.ToString("HH:mm:ss");
+
+            return View("DebugData");
+        }
+
+        public ViewResult List()
+        {
+            return View();
+        }
+    }
+}
